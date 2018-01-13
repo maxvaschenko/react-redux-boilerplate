ce6d0fb2f0217b09d4f24ad7a2f1cc5cea333ede
maxim <maxvaschenko@gmail.com>
2018-01-13 14:20:15 +0200

    header add

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
+import {__Header__} from "./styled";
+
+const Header = (props) => {
+    return (
+        <__Header__>
+            <header>
+                <p>Lorem Ipsum - best radio station</p>
+                <Link to="/donate">
+                    <div className="donate">
+                        <p>Donate us</p>
+                        <i className="fa fa-money" aria-hidden="true"/>
+                    </div>
+                </Link>
+            </header>
+        </__Header__>
+    )
+};
+
+
+//TODO PropTypes
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const __Header__ = styled.div`
+  background-color: #ffffff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  margin: 0 auto;
+  box-shadow: 0px -2px 16px -2px rgba(168,159,168,1);
+  z-index: 9999;
+  header{
+    display: flex;
+    width: 90%;
+    height: 50px;
+    margin: 0 auto;
+    justify-content: space-between;
+    align-items: center;
+    .donate{
+      display: flex;
+      align-items: center;
+      p{
+        margin-right: 6px;
+      }
+    }
+  }
+`;
+
+export {__Header__};
